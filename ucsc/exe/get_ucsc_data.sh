07890cb6ffa8511b865889c36ec01c2d9b14e211
ecapriotti <emidio.capriotti@gmail.com>
2016-04-17 11:01:18 -0500

    Updated get_ucsc_data.sh

---
+++ ucsc/exe/get_ucsc_data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+path=`dirname $0`/..
+if [[ -z "$1" ]]
+then
+  hg="all"
+else
+  hg="$hg"
+fi
+
+
+if [[ "$hg" == "all" || "$hg" == "hg19" ]]
+then
+  /bin/sh $path/hg19/get_hg19.sh
+fi
+
+if [[ "$hg" == "all" || "$hg" == "hg38" ]]
+then
+  /bin/sh $path/hg38/get_hg38.sh
+fi
