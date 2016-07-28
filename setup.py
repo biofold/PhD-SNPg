726cceb22eff3b33ccbb3d71a1cca6690ad6c4d8
ecapriotti <emidio.capriotti@gmail.com>
2016-07-28 05:40:25 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -15,27 +15,27 @@ def get_ucsc_tools(arch_type):
 	wtwobit=ucsc_tool+'/'+arch_type+'/'+twobit
 	cmd=prog_get+' '+wtwobit+' -O '+ucsc_dir+'/exe/'+twobit+'; chmod a+x '+ucsc_dir+'/exe/'+twobit
 	print >> sys.stderr,'   Download twoBitToFa'
-	print cmd
+	print 'CMD:',cmd
 	out=getstatusoutput(cmd)
 	print out[1]
 	wbwg=ucsc_tool+'/'+arch_type+'/'+bwg
 	cmd=prog_get+' '+wbwg+' -O '+ucsc_dir+'/exe/'+bwg+'; chmod a+x '+ucsc_dir+'/exe/'+bwg
 	print >> sys.stderr,'   Download bigWigToBedGraph'
-	print cmd
+	print 'CMD:',cmd
 	out=getstatusoutput(cmd)
 	print out[1]
 	print >> sys.stderr,'   Test tools'
 	cmd=ucsc_dir+'/exe/twoBitToFa'
-	print cmd
+	print 'CMD:',cmd
 	out=getstatusoutput(cmd)
 	print out[1]
 	if out[0]!=0 and out[0]!=65280:
 		print >> sys.stderr,'  ERROR: Incorrect architecture',arch_type
 		cmd='rm '+ucsc_dir+'/exe/twoBitToFa'
-		getstatusoutput(cmd)
+		
 		return out
 	cmd=ucsc_dir+'/exe/bigWigToBedGraph'
-	print cmd
+	print 'CMD:',cmd
 	out=getstatusoutput(cmd)
 	print out[1]
 	if out[0]!=0 and out[0]!=65280:
@@ -54,9 +54,9 @@ def get_ucsc_data(hg,namefile,odir,ucsc_dat='http://hgdownload.cse.ucsc.edu/gold
 	print '\n   Download',namefile
 	data = ucsc_dat+'/'+hg+'/'+odir+'/'+namefile
 	cmd = prog_get+' '+data+' -O '+ucsc_dir+'/'+namefile
-	print cmd
+	print 'CMD:',cmd
 	out=getstatusoutput(cmd)
-	print cmd
+	print 'CMD:',cmd
 	if not os.path.isfile(ucsc_dir+'/'+namefile): 
 		print >> sys.stderr,'ERROR: Not found ',namefile
 		sys.exit(1)
@@ -68,7 +68,7 @@ def setup(arch_type,hg='all',web=False):
 	prog_get = 'wget'
 	print '1) Check wget'
 	cmd='which '+prog_get
-	print cmd
+	print 'CMD:',cmd
 	out=getstatusoutput(cmd)
 	print out[1]
 	if out[0]!=0:
@@ -77,14 +77,14 @@ def setup(arch_type,hg='all',web=False):
 	print '\n2) Compile scikit-learn-0.17 and check joblib'
         cmd='cd '+prog_dir+'/tools/; tar -xzvf scikit-learn-0.17.tar.gz;' 
 	cmd=cmd+'cd scikit-learn-0.17; python setup.py install --install-lib='+prog_dir+'/tools'
-	print cmd
+	print 'CMD:',cmd
         out=getstatusoutput(cmd)
         print out[1]
         if out[0]!=0:
                 print >> sys.stderr, "ERROR: scikit-learn istallation failed."
                 sys.exit(1)	
         cmd='python -c \'from sklearn.externals import joblib\''
-	print cmd
+	print 'CMD:',cmd
 	out=getstatusoutput(cmd)
 	print out[1]
 	if out[0]!=0:
@@ -137,43 +137,37 @@ def test(hg='all',web=False):
 	test_dir = prog_dir+'/test'
 	ucsc_tool =  ucsc_dir+'/exe'
 	print '1) Test python libray scikit-learn and load prediction models'
-	cmd='cd '+prog_dir+'/tools; python -c \'from sklearn.externals import joblib\''
-	print cmd
-	out=getstatusoutput(cmd)
-	if out[0]==0:
-		print 'Loaded joblib library from sklearn.externals.'
-	else:
-		print >> sys.stderr,'ERROR: scikit-learn not installed'
-		sys.exit(1)
-	
-	models=[prog_dir+'/data/model/snv_model_w5_p7_500.pkl',prog_dir+'/data/model/indel_model_w5_p7_500.pkl']
-	try:
-		from sklearn.externals import joblib
-		print ''
+	for ihg in hgs:
+		models=[prog_dir+'/data/model/snv_model_w5_p7_500_'+ihg+'.pkl',prog_dir+'/data/model/indel_model_w5_p7_500_'+ihg+'.pkl']
 		for model in models:
-			omodel=joblib.load(model)
-			print >> sys.stderr,'Loaded model',model
-	except:
-		print >> sys.stderr,'ERROR: Not able to load prediction models'
-		sys.exit(1)
+			cmd='cd '+prog_dir+'/tools; python -c \'from sklearn.externals import joblib; joblib.load("'+model+'")\''
+			print 'CMD:',cmd
+			out=getstatusoutput(cmd)
+			if out[0]==0:
+				print 'Loaded joblib model',model
+			else:
+				print >> sys.stderr,'ERROR: Not able to load prediction models.'
+				sys.exit(1)
 
 	print '\n2) Test zcat command'
 	cmd='zcat -f '+test_dir+'/test_variants_hg19.vcf.gz '
-	print cmd
+	print 'CMD:',cmd
 	out=getstatusoutput(cmd+' |grep -A 2 \'#CHROM\'')
 	print out[1]
 	if out[0]!=0:
-		print >> sys.stderr,'ERROR: Command zcat not available'
+		print >> sys.stderr,'ERROR: Command zcat not available.'
 		sys.exit(1)
 
 	if web:	
-		if hg=='all' or hg=='hg19':
-			ihg='hg19'
-			print '\n3) Check web hg19 files'
-			hg19s=[ucsc_path+'/'+ihg+'/bigZips/'+ihg+'.2bit', \
-				biofold_path+'/'+ihg+'/'+ihg+'.phyloP46way.primate.bw',\
-				ucsc_path+'/'+ihg+'/phyloP100way/'+ihg+'.100way.phyloP100way.bw']
-			for hgfile in hg19s:
+		print '\n3) Check web hg19 and/or hg38 files'
+		dhg = {'hg19':[ucsc_path+'/hg19/bigZips/hg19.2bit', \
+			biofold_path+'/hg19/hg19.phyloP46way.primate.bw',\
+			ucsc_path+'/hg19/phyloP100way/hg19.100way.phyloP100way.bw'],\
+			'hg38':[ ucsc_path+'/hg38/bigZips/hg38.2bit', \
+			ucsc_path+'/hg38/phyloP7way/hg38.phyloP7way.bw',\
+			ucsc_path+'/hg38/phyloP100way/hg38.phyloP100way.bw']}
+		for ihg in hgs:
+			for hgfile in dhg[ihg]:
 				cmd='curl --head -s '+hgfile
 				out=getstatusoutput(cmd)
 				if out[1].find('HTTP/1.1 200 OK')==-1:
@@ -182,60 +176,33 @@ def test(hg='all',web=False):
 				else:
 					print 'File',hgfile,'available.'
 
-		if hg=='all' or hg=='hg38':
-			ihg='hg38'
-			print '\n4) Check web hg38 files'	
-			hg38s=[ ucsc_path+'/'+ihg+'/bigZips/'+ihg+'.2bit', \
-				ucsc_path+'/'+ihg+'/phyloP7way/'+ihg+'.phyloP7way.bw',\
-				ucsc_path+'/'+ihg+'/phyloP100way/'+ihg+'.phyloP100way.bw']
-			for hgfile in hg38s:
-				cmd='curl --head -s '+hgfile
-				out=getstatusoutput(cmd)			
-				if out[1].find('HTTP/1.1 200 OK')==-1:
-					print >> sys.stderr,'ERROR: Data file',hgfile,'not available.'
-					sys.exit(1)
-				else:
-					print 'File',hgfile,'available.'
 	else:
-		if hg=='all' or hg=='hg19':
-			print '\n3) Check local hg19 files. Please wait, md5sum can take few minutes.'
-        		cmd='cd '+ucsc_dir+'/hg19/; md5sum -c hg19.md5'
-			print cmd
+		print '\n3) Check local hg19 and/or hg38 files. Please wait, md5sum can take few minutes.'
+		for ihg in hgs:
+        		cmd='cd '+ucsc_dir+'/'+ihg+'/; md5sum -c '+ihg+'.md5'
+			print 'CMD:',cmd
 			out=getstatusoutput(cmd)
 			print out[1]
 			if out[0]!=0:
-				print >> sys.stderr,'WARNING: md5sum failed on hg19'
-				print >> sys.stderr,'hg19 predictions will run only in web mode '
+				print >> sys.stderr,'WARNING: md5sum failed on '+ihg
+				print >> sys.stderr,ihg+' predictions will run only in web mode '
 		
-		if hg=='all' or hg=='hg38':
-			print '\n4) Check local hg38 files. Please wait, md5sum can take few minutes.'
-			cmd='cd '+ucsc_dir+'/hg38/; md5sum -c hg38.md5'
-			print cmd
-			out=getstatusoutput(cmd)
-			print out[1]
-			if out[0]!=0:
-				print >> sys.stderr,'WARNING: md5sum failed on hg38'
-				print >> sys.stderr,'hg38 predictions will run only in web mode '
 
-	#if hgs==[]:
-	#	print >> sys.stderr,'ERROR: UCSC data file not correctly downloaded'
-	#	sys.exit()
-
-	print '\n5) Test twoBitToFa command'
+	print '\n4) Test twoBitToFa command'
 	for ihg in hgs:	
 		if web:
-			twobit=ucsc_path+'/'+ihg+'/bigZips/'+ihg+'.2bit'
+			twobit=ucsc_path+'/'+ihg+'/bigZips/'+ihg+'.2bit'	
 		else:
 			twobit=ucsc_dir+'/'+ihg+'/'+ihg+'.2bit'
 		cmd=ucsc_tool+'/twoBitToFa '+twobit+' stdout -seq=chr1 -start=10008 -end=10010'
-		print cmd
+		print 'CMD:',cmd
 		out=getstatusoutput(cmd)
 		print out[1]
 		if out[0]!=0:
 			print >> sys.stderr,'ERROR: twoBitToFa not working with',ihs
 			sys.exit(1)
 
-	print '\n6) Test bigWigToBedGraph command'
+	print '\n5) Test bigWigToBedGraph command'
 	for ihg in hgs:
 		if web:
 			if ihg=='hg19':
@@ -248,20 +215,20 @@ def test(hg='all',web=False):
 			else:
 				bwg=ucsc_dir+'/'+ihg+'/'+ihg+'.phyloP100way.bw'
 		cmd=ucsc_tool+'/bigWigToBedGraph '+bwg+' stdout -chrom=chr1 -start=100008 -end=100012'
-		print cmd
+		print 'CMD:',cmd
 		out=getstatusoutput(cmd)
 		print out[1]
 		if out[0]!=0:
 			print >> sys.stderr,'ERROR: bigWigToBedGraph not working with',ihg
 			sys.exit(1)
 
-	print '\n7) Test predict_variants.py'
+	print '\n6) Test predict_variants.py'
 	for ihg in hgs:
 		if web:
 			cmd='python predict_variants.py test/test_short_variants_'+ihg+'.tsv -g '+ihg+' --web '
 		else:
 			cmd='python predict_variants.py test/test_short_variants_'+ihg+'.tsv -g '+ihg
-		print cmd
+		print 'CMD:',cmd
 		out=getstatusoutput(cmd)
 		print out[1]
 		if out[0]!=0:
