1287d02e2b5cc9f068b405975e95e4496fae6fd2
Emidio Capriotti <emidio.capriotti@gmail.com>
2017-12-12 12:21:20 +0100

    Update get_hg38.sh

--- ucsc/hg38/get_hg38.sh
+++ ucsc/hg38/get_hg38.sh
@@ -9,6 +9,6 @@ path=`dirname $0`
 echo '- Download hg38.2bit'
 wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/hg38.2bit -P $path/hg38.2bit
 echo '- Download hg38.phyloP7way.bw'
-wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/phyloP100way/hg38.phyloP7way.bw -P $path/hg38.phylo7way.bw
+wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/phyloP7way/hg38.phyloP7way.bw -P $path/hg38.phylo7way.bw
 echo '- Download hg38.phyloP100way.bw'
 wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/phyloP100way/hg38.phyloP100way.bw -P $path/hg38.phyloP100way.bw
